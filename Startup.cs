bf10ecf399e468ef1cdb1dd60aa2134c05f17fa2
Jan Potočnik <jan.potocnik@pop-tv.si>
2018-05-24 15:43:25 +0200

    minor

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Services;
 
 namespace netcore_jwt
 {
@@ -24,6 +25,7 @@ namespace netcore_jwt
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<JwtService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
