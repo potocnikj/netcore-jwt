658804e57464e340dd3c72399b55811f1ba92842
erikbozic <bozic.erik@gmail.com>
2018-05-25 17:55:26 +0200

    Removes mvc, only uses middleware

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using netcore_jwt.Middleware;
 using Services;
@@ -19,7 +20,6 @@ namespace netcore_jwt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddScoped<JwtService>();
             
             services.AddSingleton(Configuration);
@@ -28,13 +28,28 @@ namespace netcore_jwt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.Map("/v1/jwt/encode", appBuilder => {
+                
+                appBuilder.UseMiddleware<ErrorHandlingMiddleware>();
+                appBuilder.Run(async context => {
+                        var data = context.Request.Query["data"];
+                        var service = context.RequestServices.GetService<JwtService>();
+                        var token = service.Encode(data);
+                        await context.Response.WriteAsync(token);
+                });
+            });
+             app.Map("/v1/jwt/decode", appBuilder => {
+                 
+                appBuilder.UseMiddleware<ErrorHandlingMiddleware>();
+                appBuilder.Run(async context => {
+                        var token = context.Request.Query["token"];
+                        var service = context.RequestServices.GetService<JwtService>();
+                        var data = service.Decode(token);
+                        await context.Response.WriteAsync(data);
+                });
+            });
+
             
-            app.UseMvc();
         }
     }
 }
