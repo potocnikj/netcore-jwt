a3e5ecc947fd4d7cc74d3654478e1441135f5471
Jan Potočnik <jan.potocnik@pop-tv.si>
2018-05-25 11:28:44 +0200

    minor changes

--- Controllers/DecodeController.cs
+++ Controllers/DecodeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Serialization;
 using Services;
--- Controllers/EncodeController.cs
+++ Controllers/EncodeController.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 using Services;
